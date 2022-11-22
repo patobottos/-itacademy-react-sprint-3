985899c784ba25594d6d936a1b38e48df81e500c
Patricio Bottos <patobottos@gmail.com>
2022-11-22 13:17:24 +0100

    Nivell 2 - Exercicis 8 i 9

--- js/shop.js
+++ js/shop.js
@@ -1,68 +1,3 @@
-// If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-var products = [
-   {
-        id: 1,
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery',
-        offer: {
-            number: 3,
-            percent: 20
-        }
-    },
-    {
-        id: 2,
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
-    },
-    {
-        id: 3,
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery',
-        offer: {
-            number: 10,
-            percent: 30
-        }
-    },
-    {
-        id: 4,
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        id: 5,
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        id: 6,
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        id: 7,
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        id: 8,
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        id: 9,
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
-    }
-]
 // Array with products (objects) added directly with push(). Products in this array are repeated.
 var cartList = [];
 
@@ -83,7 +18,6 @@ function buy(id) {
             cartList.push(products[i]);
         }
     }
-    console.log(cartList);
 }
 
 // Exercise 2
@@ -131,9 +65,10 @@ function applyPromotionsCart() {
     // STEP 1 - CREATE PROPERTY subtotalWithDiscount FOR ALL CART ITEMS
     cart.forEach((x) => {
         x.subtotalWithDiscount = x.price});
-
+           
     // PROMOTION #1 - oil discount
     let oilItemIndex = cart.findIndex(item => item.name === 'cooking oil');
+
     if (oilItemIndex !== -1 && cart[oilItemIndex].quantity >= 3) {
         cart[oilItemIndex].subtotalWithDiscount = 10;
     };
@@ -179,12 +114,64 @@ function addToCart(id) {
     // Refactor previous code in order to simplify it 
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+
+    let counter = 0;
+
+    // We look for the index of product in the product array
+    let selectedProductIndex = products.findIndex(x => x.id === id);
+    let selectedProduct = products[selectedProductIndex];
+
+    // We look for the index of selected product in cart array 
+    let cartItemIndex = cart.findIndex(item => item.id === selectedProduct.id);
+
+    // If we do not have it, we add quantity = 1, and we push it into cart array
+    if (cartItemIndex == -1) {
+        selectedProduct.quantity = 1;
+        cart.push(selectedProduct);
+
+    // If we already have it, we add 1 to its quantity
+    } else {
+        cart[cartItemIndex].quantity++;
+    }
+
+    // We update the item counter in cart icon of the menu
+    for (let i = 0; i < cart.length; i++) {
+        counter += cart[i].quantity;
+    }
+    document.getElementById('count_product').innerHTML = counter;
+
+    // We update promotions
+    applyPromotionsCart();
 }
 
 // Exercise 9
 function removeFromCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+
+    let counter = 0;
+
+    // We find the index of the item to remove
+    let indexItemtoRemove = cart.findIndex(item => item.id === id);
+    
+    // We delete item from cart, in case we have one or more
+    if (indexItemtoRemove == -1) {
+        alert("You don't have any item of this kind yet");
+    } else {
+        if (cart[indexItemtoRemove].quantity > 1) {
+            cart[indexItemtoRemove].quantity --;
+        } else {
+            cart.splice(indexItemtoRemove,1);
+            console.log('cart luego del slice ',cart);
+        }
+    }
+
+    //We update the item counter in cart icon of the menu
+    for (let i = 0; i < cart.length; i++) {
+        counter += cart[i].quantity;
+    }
+    document.getElementById('count_product').innerHTML = counter;
+
+    // We update promotions
+    applyPromotionsCart();
 }
 
 function open_modal(){
